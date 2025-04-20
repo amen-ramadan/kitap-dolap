534bba4e8823b57d317c308873a63eab16e9f3ea
Amen Ramadan <ramadanamen407@gmail.com>
2025-04-20 23:23:33 +0300

    edit api url for omran

--- src/store/modules/books/api.js
+++ src/store/modules/books/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/data";
-const API_SEARCH_URL = "http://localhost:3000/data";
+const API_URL = "https://localhost:9001/api/v1/BookListing/search";
+const API_SEARCH_URL = "https://localhost:9001/api/v1/BookListing/search";
 
 export const fetchBooks = async () => {
   const response = await axios.get(API_URL);
