56cc405b12e89582557a3165fa7c0f3a57f8d265
Amen Ramadan <ramadanamen407@gmail.com>
2025-04-25 11:55:33 +0300

    add alote of logic for favorites page and commit them2

--- src/store/modules/books/api.js
+++ src/store/modules/books/api.js
@@ -1,45 +1,55 @@
 import axios from "axios";
-import useAuthStore from "../../authStore";
+// import useAuthStore from "../../authStore";
 
 const API_SEARCH_URL = "https://localhost:9001/api/v1/";
 
-// Create axios instance
-export const api = axios.create({ baseURL: API_SEARCH_URL });
+// // Create axios instance
+// export const api = axios.create({ baseURL: API_SEARCH_URL });
 
-// Attach auth token for protected endpoints
-api.interceptors.request.use((config) => {
-  const token = useAuthStore.getState().token;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// // Attach auth token for protected endpoints
+// api.interceptors.request.use((config) => {
+//   const token = useAuthStore.getState().token;
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
 // Search books without auth
 export const fetchSearchBooks = async (params) => {
-  const { data } = await axios.get("BookListing/search", { params });
+  const { data } = await axios.get(`${API_SEARCH_URL}BookListing/search`, {
+    params,
+  });
   return data;
 };
 
-// Favorites endpoints
-export const fetchFavoritesBooks = async () => {
-  const { data } = await api.get("Favorites");
-  return data;
-};
+// // for edit in my amen desktop
+// export const fetchSearchBooks = async (params) => {
+//   const { data } = await axios.get(`${API_SEARCH_URL}BookListing/search`, {
+//     params,
+//   });
+//   return data;
+// };
 
-export const CheckFavoriteBook = async (bookId) => {
-  return await api.get(`Favorites/check/${bookId}`);
-};
+// // Favorites endpoints
+// export const fetchFavoritesBooks = async () => {
+//   const { data } = await api.get("Favorites");
+//   return data;
+// };
 
-export const AddFavorite = async (bookId) => {
-  const response = await api.post("Favorites", { bookListingId: bookId });
-  return response;
-};
+// export const CheckFavoriteBook = async (bookId) => {
+//   return await api.get(`Favorites/check/${bookId}`);
+// };
 
-export const DeleteFavorite = async (id) => {
-  const response = await api.delete(`Favorites/${id}`);
-  if (response.status === 200 || response.status === 204) {
-    return true;
-  }
-  return false;
-};
+// export const AddFavorite = async (bookId) => {
+//   const response = await api.post("Favorites", { bookListingId: bookId });
+//   return response;
+// };
+
+// export const DeleteFavorite = async (id) => {
+//   const response = await api.delete(`Favorites/${id}`);
+//   if (response.status === 200 || response.status === 204) {
+//     return true;
+//   }
+//   return false;
+// };
